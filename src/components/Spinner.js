7587173fa1942337326b64718a206bdc638cecfa
jayant636 <jayantjalandra@gmail.com>
2023-12-20 16:11:52 +0530

    Testing2

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,9 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="flex flex-col items-center space-y-2">
-      <div className="spinner custom-loader">
-        <p>Loading ...</p>
-      </div>
+  
     </div>
   );
 };
